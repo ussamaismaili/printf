eb67fea4463564e21937b0b4cbf9e9cb3e631089
Ismaili oussama <ismailiossama@gmail.com>
2023-04-27 00:09:11 -0700

    start all over

---
+++ check_spec.c
@@ -0,0 +1,25 @@
+#include "myh.h"
+#include<stdarg.h>
+#include<stdlib.h>
+/**
+ * check_spec - checks that the the specifier passed is a valid one,
+ * and the run the specific function.
+ * @frmt: the specifier .
+ * Return : If it's not a null it returns a pointer to the function.
+ */
+int (*check_spec(char *frmt))(va_list)
+{
+	fun_t array[4] ={
+		{"c", _putchar},
+		{"s", _putstr},
+		{"%", _percent},
+		{NULL , NULL}};
+	}
+	int i;
+	for (i = 0 ; array[i].cm != NULL ; i++)
+		if (*(array[i].cm) == *frmt)
+		{
+			return (array[i].f);
+		}
+	return (NULL);
+}
--- main.h
+++ main.h
@@ -1,23 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include<stdarg.h>
+#ifndef _MYH_H_
+#define _MYH_H_
 
-int _printf(const char *format, ...);
+int _printf(const char *frmt, ...);
 int _putchar(char c);
 int _putstr(char *str);
-void print_all(char c, va_list arg, int *len);
-void print_num(int number, int *len);
-void print_bin(unsigned int number, int *len);
-void print_unsi_num(unsigned int number, int *len);
-void print_octal_num(unsigned int number, int *len);
-void print_hex_num(unsigned int number, char c, int *len);
-int _putchar_buff(char c);
-void print_str(char *str, int *len);
-void print_adrs(unsigned long i, char *s, int *len);
-void reve_print(char *str, int *len);
-void rot(char *str, int *len);
-void print_plus(int num, int *len);
-void print_space(int num, int *len);
-void print_prefix(int num, char c, int *len);
+int (*check_spec(char*))(val_list);
+/**
+ * struct fun - the struct for the specifier to the pointer.
+ * @cm: charcter to be compared
+ * @f: function handling to the specific printing type.
+ */
+typedef struct fun
+{
+	char *cm;
+	int (*f)(val_list):
+} fun_p;
 
 #endif
