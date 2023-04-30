e4be16b8cead5cd078f94e57ad95023143aee8ae
Ismaili oussama <ismailiossama@gmail.com>
2023-04-30 02:31:38 -0700

    Im siiiick

--- prt_str.c
+++ prt_str.c
@@ -5,7 +5,7 @@
  * Return: the  number of characters printed on Success or -1 on failure .
  */
 int prt_str(va_list args)
-{
+{	
 	int i = 0;
 	int cnt = 0;
 	char *str;
@@ -13,15 +13,14 @@ int prt_str(va_list args)
 	str = va_arg(args, char *);
 	if (str == NULL)
 	{
-		cnt = write(1, "(null)", 6);
+        	cnt = write(1, "(null)", 6);
+		return (cnt);
 	}
-	else
+
+	while (str[i])
 	{
-		while (str[i])
-		{
-			cnt = write(1, &str[i], 1);
-			i = i + 1;
-		}
-	}
+		cnt = write(1, &str[i], 1);
+		i = i + 1;
+ 	}
 	return (cnt);
 }
