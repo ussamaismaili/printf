06707dae896d7a46e2c9e56ae24f4b738c843188
Ismaili oussama <ismailiossama@gmail.com>
2023-04-30 21:42:57 -0700

    hehf

--- prt_hex2.c
+++ prt_hex2.c
@@ -1,35 +1,26 @@
-#include "main.h"
-/**
- * prt_hex2 - prints an hexgecimal number too.
- * @num: the arguments.
- * Return: the nu,be counter.
- */
 int prt_hex2(unsigned long int num)
 {
-	long int i;
-	long int *arr;
-	long int cnt = 0;
-	unsigned long int temp = num;
+    int cnt = 0;
+    int i;
+    int arr[20];
 
-	while (num / 16 != 0)
-	{
-		num /= 16;
-		cnt++;
-	}
-	cnt++;
-	arr = malloc(cnt * sizeof(long int));
+    if (num == 0) {
+        _putchar('0');
+        return 1;
+    }
 
-	for (i = 0; i < cnt; i++)
-	{
-		arr[i] = temp % 16;
-		temp /= 16;
-	}
-	for (i = cnt - 1; i >= 0; i--)
-	{
-		if (arr[i] > 9)
-			arr[i] = arr[i] + 55;
-		_putchar(arr[i] + '0');
-	}
-	free(arr);
-	return (cnt);
+    while (num > 0) {
+        arr[cnt++] = num % 16;
+        num /= 16;
+    }
+
+    for (i = cnt - 1; i >= 0; i--) {
+        if (arr[i] < 10) {
+            _putchar(arr[i] + '0');
+        } else {
+            _putchar(arr[i] - 10 + 'A');
+        }
+    }
+
+    return cnt;
 }
