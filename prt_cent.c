23557f808e75759f3740743b7150e879215e38a5
Ismaili oussama <ismailiossama@gmail.com>
2023-04-30 21:57:15 -0700

    hehfhh99

--- prt_cent.c
+++ prt_cent.c
@@ -8,6 +8,6 @@ int prt_cent(va_list args)
 {
 	(void)args;
 
-	write(1, "%", 1);
+	write(1, "%%", 1);
 	return (1);
 }
--- prt_hex2.c
+++ prt_hex2.c
@@ -6,27 +6,30 @@
  */
 int prt_hex2(unsigned long int num)
 {
-    int cnt = 0;
-    int i;
-    int arr[20];
+	long int i;
+	long int *arr;
+	long int cnt = 0;
+	unsigned long int temp = num;
 
-    if (num == 0) {
-        _putchar('0');
-        return 1;
-    }
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		cnt++;
+	}
+	cnt++;
+	arr = malloc(cnt * sizeof(long int));
 
-    while (num > 0) {
-        arr[cnt++] = num % 16;
-        num /= 16;
-    }
-
-    for (i = cnt - 1; i >= 0; i--) {
-        if (arr[i] < 10) {
-            _putchar(arr[i] + '0');
-        } else {
-            _putchar(arr[i] - 10 + 'A');
-        }
-    }
-
-    return cnt;
+	for (i = 0; i < cnt; i++)
+	{
+		arr[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = cnt - 1; i >= 0; i--)
+	{
+		if (arr[i] > 9)
+			arr[i] = arr[i] + 55;
+		_putchar(arr[i] + '0');
+	}
+	free(arr);
+	return (cnt);
 }
